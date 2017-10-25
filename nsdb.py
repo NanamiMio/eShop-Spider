eb37233cbf26cc51e175dbb6f5d1389fd252468c
shower <zhangshower@gmail.com>
2017-10-25 23:32:14 +0800

    fix JP

--- nsdb.py
+++ nsdb.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+import re
 
 import requests
 import xmltodict
@@ -12,6 +13,7 @@ GET_GAMES_EU_URL = "http://search.nintendo-europe.com/en/select"
 GET_GAMES_JP_CURRENT = "https://www.nintendo.co.jp/data/software/xml-system/switch-onsale.xml"
 GET_GAMES_JP_COMING = "https://www.nintendo.co.jp/data/software/xml-system/switch-coming.xml"
 GET_GAMES_JP_ALT = "https://www.nintendo.co.jp/api/search/title?category=products&pf=switch&q=*&count=25"
+GUESS_GAMES_GP_URL = 'https://ec.nintendo.com/JP/ja/titles/'
 GET_PRICE_URL = "https://api.ec.nintendo.com/v1/price?lang=en"
 
 GAME_LIST_LIMIT = 200
@@ -21,7 +23,8 @@ GAME_CODE_REGEX_JP = r'\/HAC(\w{4})'
 GAME_CODE_REGEX_US = r'HAC\w(\w{4})'
 GAME_CODE_REGEX_EU = r'HAC\w(\w{4})'
 
-NSUID_REGEX_JP = '\d{14}'
+NSUID_REGEX_JP = r'\d{14}'
+JSON_REGEX = r'NXSTORE\.titleDetail\.jsonData = ([^;]+);'
 
 GAME_CHECK_CODE_US = 70010000000185
 GAME_CHECK_CODE_EU = 70010000000184
@@ -33,7 +36,7 @@ REGION_AMERICA = "AG AI AR AW BB BM BO BR BS BZ CA CL CO CR DM DO EC GD GF GP GT
 
 COUNTRIES = "AT AU BE BG CA CH CY CZ DE DK EE ES FI FR GB GR HR HU IE IT JP LT LU LV MT MX NL NO NZ PL PT RO RU SE SI SK US ZA".split(' ')
 
-FIRST_NSUID = 70010000000000
+FIRST_NSUID = 70010000000026
 
 def getGamesAmerica(offset = 0, games=[]):
     #print(offset, len(games))
@@ -64,6 +67,21 @@ def getGamesJapan():
     
     gamesCurrent.extend(gamesComing)
     return gamesCurrent
+
+def guessGamesJapan():
+    games = []
+    for i in range(FIRST_NSUID, FIRST_NSUID + 1500):
+        r = requests.get(GUESS_GAMES_GP_URL + str(i))
+        if r.status_code is 200:
+            game = json.loads(re.search(JSON_REGEX, r.text).group(1))
+            games.append(game)
+            print(i, r.status_code)
+        # else:
+        #     print(i, r.status_code)
+
+    return games
+
+
 
 def getGamesEurope():   
     params = {
@@ -162,11 +180,38 @@ def getPrices(country, gameIds, offset = 0, prices = []):
         'prices': prices
     }
     return response
+
+def parseGameCode(game, region):
+
+    code = ""
+
+    if region is "Europe" and 'product_code_txt' in game:
+        code = re.match(GAME_CODE_REGEX_EU, game['product_code_txt'][0]).group(1)
+    if region is "Asia" and 'ScreenshotImgURL' in game:
+        code = re.match(GAME_CODE_REGEX_JP, game['ScreenshotImgURL'][0]).group(1)
+    if region is "America" and 'game_code' in game:
+        code = re.match(GAME_CODE_REGEX_US, game['game_code']).group(1)
+
+    return code
+
+
+def parseNSUID(game, region):
+
+    nsuid = ""
+
+    if region is "Europe" and 'nsuid_txt' in game:
+        nsuid = game['nsuid_txt']
+    if region is "Asia" and 'LinkURL' in game:
+        nsuid = re.match(NSUID_REGEX_JP, game['LinkURL'][0]).group(1)
+    if region is "America" and 'nsuid' in game:
+        nsuid =  game['nsuid']
+
+    return nsuid
         
 
 
 if __name__=='__main__':
-    result = getShops()
-    print(result, len(result))
+    result = guessGamesJapan()
+    print(result[0], len(result))
     # with open('result.json', 'w') as f:
         # f.write(result)
